f4a200d35dcb8f97ff42926e22d305ff49ef3b88
Trevnado <tlmcmu02@harrisonburg.k12.va.us>
2019-09-17 09:10:52 -0400

    Update game.js

--- game.js
+++ game.js
@@ -3,12 +3,30 @@ var score = 0;
 postToPage(score);
 
 createButton('Button', btnPress);
+createButton('Button', btn2Press);
+createButton('Button', btn3Press);
+createButton('Button', btn4Press);
 changeTitle('Clicker Game');
 
 setInterval(btnPress, 1000);
 
+score--;
+
 function btnPress() {
   score++;
   postToPage(score);
+}
+
+function btn2Press() {
+  score--;
+  postToPage(score);
+}
+
+if (score > 50){
+  function btn3Press() {
+    score++;
+    postToPage(score);
+  }
+  
 }
 
