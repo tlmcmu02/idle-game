a10ca4efe5cd76ac27dab1357f1ce646f4d79e1a
Trevnado <tlmcmu02@harrisonburg.k12.va.us>
2019-09-24 09:17:48 -0400

    Update game.js

--- game.js
+++ game.js
@@ -3,9 +3,9 @@ var stone = 0;
 var iron = 0;
 var redstone = 0;
 var diamond = 0;
-
-
-var item = 0;
+var craft = 0;
+var stick = 0;
+var explore = 0;
 
 var title = new Title('Welcome to the Game'); // Create a title element
 
@@ -14,9 +14,14 @@ var gatherstone = new Button('Stone', btn2Press);
 var gatheriron = new Button('Iron', btn3Press);
 var gatherredstone = new Button('Copper',btn4Press);
 var gatherdiamond = new Button('Diamond', btn5Press);
-var woodfarm = new Button('+1CPS', btnPress);
+var woodfarm = new Button('Wood Farm', btnPress);
+var makecraft = new Button('Workbench', btn6Press);
+var makestick = new Button('Stick', btn7Press);
+var explorebutton = new Button('Explore', btn7Press);
 
+var crft = new Text(craft);  
 var wd = new Text(wood);                    // Create a text element
+var stck = new Text(stick);
 var stn = new Text(stone); 
 var irn = new Text(iron);  
 var rdstn = new Text(redstone); 
@@ -27,44 +32,66 @@ gatheriron.hide();
 gatherredstone.hide();
 gatherdiamond.hide();
 woodfarm.hide();
+makecraft.hide();
+makestick.hide();
 
 function btn1Press() {
   wood++;         // Increase the score
   wd.edit(wood); // Update the page with the new score
-  postToPage(wood);
+}
+
+function btn7Press() {
+  stick++;         // Increase the score
+  stck.edit(stick); // Update the page with the new score
+}
+
+
+function btn6Press() {
+  craft++;         // Increase the score
+  crft.edit(craft); // Update the page with the new score
 }
 
 function btn2Press() {
   stone++;         // Increase the score
   stn.edit(stone); // Update the page with the new score
-  postToPage(stone);
 }
 
 function btn3Press() {
   iron++;         // Increase the score
   irn.edit(iron); // Update the page with the new score
-  postToPage(iron);
 }
 
 function btn4Press() {
   redstone++;         // Increase the score
   rdstn.edit(redstone); // Update the page with the new score
-  postToPage(redstone);
 }
 
 function btn5Press() {
   diamond++;         // Increase the score
   dmnd.edit(diamond); // Update the page with the new score
-  postToPage(diamond);
 }
 
-setInterval(checkstn, 100);
-setInterval(checkrdstn, 100);
-setInterval(checkirn, 100);
+setInterval(checkstck, 100);
 setInterval(checkwd, 100);
+setInterval(checkcrft, 100);
+setInterval(checkstn, 100);
+setInterval(checkirn, 100);
+setInterval(checkrdstn, 100);
+
+function checkstck(){
+  if (wood > 2){
+    makestick.show();
+  }
+}
 
 function checkwd(){
-  if (wood > 50){
+  if (wood > 4){
+    makecraft.show();
+  }
+}
+
+function checkcrft(){
+  if (craft > 0){
     gatherstone.show();
   }
 }
@@ -89,9 +116,10 @@ function checkrdstn(){
 }
 
 function btnPress() {
-  if (wood > 49){
-    wood = wood - 50 
+  if (redstone > 49) {
+  redstone = redstone - 50 
     setInterval(btn1Press, 1000);
-    postToPage(wood);
+    wd.edit(wood);
+    rdstn.edit(redstone);
   }
 }
